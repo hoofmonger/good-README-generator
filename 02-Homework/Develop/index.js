bd2d596f25f0ab21b4231412c948f4137ab03736
James Hoofard <james.hoofard.iii@gmail.com>
2021-08-25 09:17:27 -0500

    it works!

--- 02-Homework/Develop/index.js
+++ 02-Homework/Develop/index.js
@@ -33,15 +33,6 @@ name: 'testDescription'},
 {message: "Select a License",
 type: 'list',
 choices: ["MIT", 
-"Apache License 2.0", 
-"GNU General Public License 3.0",
-"BSD 2-Clause Simplified License",
-"BSD 3-Clause New or Revised License",
-"Boost Software License 1.0",
-"Creative Commons Zero v1.0 Universal",
-"Eclipse Public License 2.0",
-"GNU Affero General Public License",
-"GNU Lesser General Public License v2.1",
 "Mozilla Public License 2.0",
 "The Unilicense"],
 name: 'licenseTitle'},
@@ -52,13 +43,13 @@ name: 'filename'}
 
 // TODO: Create a function to write README file
 function writeToFile(filename, data) {
-    .then((data) => {fs.appendFile(filename.data)
+   
         
     
-        fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+        fs.writeFile(filename, data, (err) =>
           err ? console.log(err) : console.log('Success!')
         );
-      })};
+      };
  
 
 
--- 02-Homework/Develop/utils/generateMarkdown.js
+++ 02-Homework/Develop/utils/generateMarkdown.js
@@ -1,14 +1,53 @@
 // TODO: Create a function that returns a licenseTitle badge based on which licenseTitle is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(licenseTitle) {}
+function renderLicenseBadge(licenseTitle) {
+if (licenseTitle === "MIT"){
+  return "https://img.shields.io/badge/MIT-hello%20world-blue"
+}
+else if (licenseTitle === "Mozilla Public License 2.0"){
+  return "https://img.shields.io/badge/Mozilla%20-Public%20License-blue"
+}
+else if (licenseTitle === "The Unilicense"){
+  return "https://img.shields.io/badge/The%20-Unilicense-blue"
+}
+else {
+  return ""
+}
+
+}
 
 // TODO: Create a function that returns the licenseTitle link
 // If there is no licenseTitle, return an empty string
-function renderLicenseLink(licenseTitle) {}
+function renderLicenseLink(licenseTitle) {
+  if (licenseTitle === "MIT"){
+    return "https://img.shields.io/badge/MIT-hello%20world-blue"
+  }
+  else if (licenseTitle === "Mozilla Public License 2.0"){
+    return "https://img.shields.io/badge/Mozilla%20-Public%20License-blue"
+  }
+  else if (licenseTitle === "The Unilicense"){
+    return "https://img.shields.io/badge/The%20-Unilicense-blue"
+  }
+  else {
+    return ""
+  }
+}
 
 // TODO: Create a function that returns the licenseTitle section of README
 // If there is no licenseTitle, return an empty string
-function renderLicenseSection(licenseTitle) {}
+function renderLicenseSection(licenseTitle) {
+  if (licenseTitle === "MIT"){
+    return "please google the license, i'm not typing out the license text in its entirety some of these are massive"
+  }
+  else if (licenseTitle === "Mozilla Public License 2.0"){
+    return "please google the license, i'm not typing out the license text in its entirety some of these are massive"
+  }
+  else if (licenseTitle === "The Unilicense"){
+    return "please google the license, i'm not typing out the license text in its entirety some of these are massive"}
+  else {
+    return ""
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -31,7 +70,7 @@ function generateMarkdown(data) {
   ## Credits
   ${data.authors}
   ## License
-  ${renderLicenseBadge(data.licenseTitle)}
+  ${renderLicenseSection(data.licenseTitle)}
   ${renderLicenseLink(data.licenseTitle)}
   
   ## Badges
